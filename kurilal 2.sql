88378c4892dc1e869a5c9f8de81b63c63f8d872a
maddrills <mathewfvv@gmail.com>
2024-09-21 21:07:13 +0530

    Data

--- kurilal 2.sql
+++ kurilal 2.sql
@@ -1,2 +1,9 @@
 USE `delivery_system`;
 
+INSERT INTO `customer`(`name`,`phone_number`) VALUES ('Joseph Kurilala', '1234567891');
+INSERT INTO `customer`(`name`,`phone_number`) VALUES ('Violet Wick', '9086410268');
+INSERT INTO `customer`(`name`,`phone_number`) VALUES ('George Nolan', '9341725638');
+INSERT INTO `customer`(`name`,`phone_number`) VALUES ('Ezio Auditore', '8097634517');
+INSERT INTO `customer`(`name`,`phone_number`) VALUES ('Arno Dorian', '8856123567');
+
+SELECT * FROM `customer`;
--- kurilal.sql
+++ kurilal.sql
@@ -6,14 +6,14 @@ USE `delivery_system`;
 
 -- YELLOW
 CREATE TABLE `customer`(
-`customer_id` INTEGER NOT NULL,
+`customer_id` INTEGER AUTO_INCREMENT NOT NULL,
 `name` VARCHAR(65) NOT NULL,
-`phone_number` CHAR(10)  CHECK (length(`phone_number`)= 10),
+`phone_number` CHAR(10) UNIQUE CHECK (length(`phone_number`)= 10),
 CONSTRAINT `pk_key_customar` PRIMARY KEY(`customer_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
 
 CREATE TABLE `order`(
-`order_id` INTEGER NOT NULL,
+`order_id` INTEGER AUTO_INCREMENT NOT NULL,
 `delivery_method` BOOLEAN,
 `quantity` INTEGER NOT NULL CHECK(`quantity` > 0),
 `address_id` INTEGER,
@@ -23,24 +23,24 @@ CONSTRAINT `pk_key_order` PRIMARY KEY(`order_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
 
 CREATE TABLE `delivery_address`(
-`address_id` INTEGER NOT NULL,
+`address_id` INTEGER AUTO_INCREMENT NOT NULL,
 `address` VARCHAR(80) NOT NULL,
 CONSTRAINT `pk_key_delivery_address` PRIMARY KEY(`address_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
 
 CREATE TABLE `track_shipments`(
-`tracking_id` INTEGER NOT NULL,
+`tracking_id` INTEGER AUTO_INCREMENT NOT NULL,
 `order_placed` BOOLEAN DEFAULT 0 NOT NULL,
 `dispatched`BOOLEAN DEFAULT 0 NOT NULL,
 `shipped` BOOLEAN DEFAULT 0 NOT NULL,
 `out_for_delivery` BOOLEAN DEFAULT 0 NOT NULL,
 `delivered` BOOLEAN DEFAULT 0 NOT NULL,
 `order_id` INTEGER NOT NULL,
-CONSTRAINT `pk_key_order` PRIMARY KEY(`order_id`)
+CONSTRAINT `pk_key_track_shipments` PRIMARY KEY(`tracking_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
 
 CREATE TABLE `payment`(
-`payment_id` INTEGER NOT NULL,
+`payment_id` INTEGER AUTO_INCREMENT NOT NULL,
 `payment_type` VARCHAR(30) NOT NULL,
 `name` VARCHAR(65) NOT NULL,
 `billing_address` VARCHAR(80) NOT NULL,
@@ -51,7 +51,7 @@ CONSTRAINT `pk_key_payment` PRIMARY KEY(`payment_id`)
 
 -- GREEN
 CREATE TABLE `orders_products`(
-`product_id` INTEGER NOT NULL,
+`product_id` INTEGER AUTO_INCREMENT NOT NULL,
 `order_id` INTEGER NOT NULL,
 CONSTRAINT `pk_key_orders_products_product_id` PRIMARY KEY(`product_id`,`order_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
@@ -59,32 +59,32 @@ CONSTRAINT `pk_key_orders_products_product_id` PRIMARY KEY(`product_id`,`order_i
 
 -- BLUE
 CREATE TABLE `products`(
-`product_id` INTEGER NOT NULL,
+`product_id` INTEGER AUTO_INCREMENT NOT NULL,
 `product_name` VARCHAR(65) NOT NULL,
 CONSTRAINT `pk_key_products_product_id` PRIMARY KEY(`product_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
 
 CREATE TABLE `category`(
-`product_id` INTEGER NOT NULL,
+`product_id` INTEGER AUTO_INCREMENT NOT NULL,
 `order_id` VARCHAR(65) NOT NULL,
 `phone_number` CHAR(10)  CHECK (length(`phone_number`)= 10),
 CONSTRAINT `pk_key_customar` PRIMARY KEY(`product_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
 
 CREATE TABLE `products_supplier`(
-`product_id` INTEGER NOT NULL,
+`product_id` INTEGER AUTO_INCREMENT NOT NULL,
 `supplier_id` INTEGER NOT NULL,
 CONSTRAINT `pk_key_products_supplier_product_id` PRIMARY KEY(`product_id`,`supplier_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
 
 CREATE TABLE `supplier`(
-`supplier_id` INTEGER NOT NULL,
+`supplier_id` INTEGER AUTO_INCREMENT NOT NULL,
 `supplier_name` VARCHAR(65) NOT NULL,
 CONSTRAINT `pk_key_supplier` PRIMARY KEY(`supplier_id`)
 )ENGINE = 'Innodb' AUTO_INCREMENT = 1, DEFAULT CHARSET 'latin1';
 
 CREATE TABLE `product_images`(
-`image_id` INTEGER NOT NULL,
+`image_id` INTEGER AUTO_INCREMENT NOT NULL,
 `url` VARCHAR(80) NOT NULL,
 `image` BLOB,
 `product_id` INTEGER NOT NULL,
